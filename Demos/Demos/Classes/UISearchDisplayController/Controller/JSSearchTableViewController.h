6963e708adbbc0c4ccd8e9db56987931fdc3e240
ShenYj <shenyanjie123@foxmail.com>
2016-09-02 12:56:43 +0800

    导入YYModel和MJRefresh

---
+++ Demos/Demos/Classes/UISearchDisplayController/Controller/JSSearchTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  JSSearchTableViewController.h
+//  Demos
+//
+//  Created by ShenYj on 16/9/1.
+//  Copyright © 2016年 ___ShenYJ___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSSearchTableViewController : UITableViewController
+
+@end
