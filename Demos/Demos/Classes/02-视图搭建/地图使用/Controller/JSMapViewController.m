e38f127aad208eea9732957b2b699c1dd6c96793
ShenYj <shenyj4@51nb.com>
2016-09-07 10:55:57 +0800

    代码逻辑处理

--- Demos/Demos/Classes/02-视图搭建/地图使用/Controller/JSMapViewController.m
+++ Demos/Demos/Classes/02-视图搭建/地图使用/Controller/JSMapViewController.m
@@ -388,36 +388,59 @@
     
     CLGeocoder *geocoder = [[CLGeocoder alloc] init];
     
-    switch (navigationInfoView.inputViewType) {
+    
+    
+    [geocoder geocodeAddressString:destinationString completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+        
+        if (error || placemarks.count == 0 ) {
+            NSLog(@"地标转换失败:%@",error);
+            return ;
+        }
+        
+        // 当前地图项目
+        MKMapItem *currentMapItem = [MKMapItem mapItemForCurrentLocation];
+        
+        // 目标地图项目
+        CLPlacemark *placeMark_cl = [[CLPlacemark alloc] initWithPlacemark:placemarks.lastObject];
+        MKPlacemark *placeMark_mk = [[MKPlacemark alloc] initWithPlacemark:placeMark_cl];
+        MKMapItem *destinationMapItem = [[MKMapItem alloc] initWithPlacemark:placeMark_mk];
+        
+        
+        // 打开内置地图进行导航
+        if (navigationInfoView.inputViewType == JSNavigationInputViewTypeSystemType) {
             
-        case JSNavigationInputViewTypeSystemType:
-            [geocoder geocodeAddressString:destinationString completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
-                
-                if (error || placemarks.count == 0 ) {
-                    NSLog(@"地标转换失败:%@",error);
-                    return ;
-                }
-                
-                // 当前地图项目
-                MKMapItem *currentMapItem = [MKMapItem mapItemForCurrentLocation];
-                
-                // 目标地图项目
-                CLPlacemark *placeMark_cl = [[CLPlacemark alloc] initWithPlacemark:placemarks.lastObject];
-                MKPlacemark *placeMark_mk = [[MKPlacemark alloc] initWithPlacemark:placeMark_cl];
-                MKMapItem *destinationMapItem = [[MKMapItem alloc] initWithPlacemark:placeMark_mk];
-                
-                // 打开内置地图进行导航
-                [MKMapItem openMapsWithItems:@[currentMapItem,destinationMapItem] launchOptions:@{MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving}];
-                
-            }];
-            break;
-        case JSNavigationInputViewTypeCustomType:
             
-            break;
+            [MKMapItem openMapsWithItems:@[currentMapItem,destinationMapItem] launchOptions:@{MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving}];
             
-        default:
-            break;
-    }
+            return;
+            
+        }
+        
+        // 使用自定义的地图进行导航  (JSNavigationInputViewTypeCustomType)
+        MKDirectionsRequest *directionRequest = [[MKDirectionsRequest alloc] init];
+        directionRequest.source = currentMapItem;
+        directionRequest.destination = destinationMapItem;
+        MKDirections *direction = [[MKDirections alloc] initWithRequest:directionRequest];
+        
+        [direction calculateDirectionsWithCompletionHandler:^(MKDirectionsResponse * _Nullable response, NSError * _Nullable error) {
+           
+            if (error || response == nil) {
+                NSLog(@"导航失败:%@",error);
+                return ;
+            }
+            
+            
+            
+            
+        }];
+        
+        
+        
+    }];
+    
+    
+    
+
     
     
     
