a99ef1019ba11d580ad8ca598a00149da132ac4f
ShenYj <shenyj4@51nb.com>
2016-09-09 19:47:33 +0800

    第三方登录(Sina)

--- Demos/Demos/Classes/02-视图搭建/友盟分享/Controller/JSSocialViewController.m
+++ Demos/Demos/Classes/02-视图搭建/友盟分享/Controller/JSSocialViewController.m
@@ -20,6 +20,8 @@
 
 @property (nonatomic,strong) UIButton *sharedButton;
 
+@property (nonatomic,strong) UIButton *loginButton;
+
 @end
 
 @implementation JSSocialViewController
@@ -38,15 +40,22 @@
     self.view.backgroundColor = [UIColor whiteColor];
     
     [self.view addSubview:self.sharedButton];
+    [self.view addSubview:self.loginButton];
     
     [self.sharedButton mas_makeConstraints:^(MASConstraintMaker *make) {
         make.centerX.centerY.mas_equalTo(self.view);
         make.size.mas_equalTo(CGSizeMake(100, 40));
     }];
     
+    [self.loginButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.mas_equalTo(self.view);
+        make.size.mas_equalTo(CGSizeMake(100, 40));
+        make.top.mas_equalTo(self.sharedButton.mas_bottom).mas_offset(20);
+    }];
 }
 
 
+// 分享 (分享到Sina)
 - (void)sharedByUMSocial{
     
     //如果需要分享回调，请将delegate对象设置self，并实现下面的回调方法
@@ -56,6 +65,28 @@
                                      shareImage:[UIImage imageNamed:@"icon"]
                                 shareToSnsNames:@[UMShareToSina]
                                        delegate:self];
+}
+
+
+// 第三方登录 (Sina)
+- (void)singleSignOnBySina{
+    
+    UMSocialSnsPlatform *snsPlatform = [UMSocialSnsPlatformManager getSocialPlatformWithName:UMShareToSina];
+    
+    snsPlatform.loginClickHandler(self,[UMSocialControllerService defaultControllerService],YES,^(UMSocialResponseEntity *response){
+        
+        // 获取微博用户名、uid、token等
+        if (response.responseCode == UMSResponseCodeSuccess) {
+            
+            NSDictionary *dict = [UMSocialAccountManager socialAccountDictionary];
+            
+            UMSocialAccountEntity *snsAccount = [[UMSocialAccountManager socialAccountDictionary] valueForKey:snsPlatform.platformName];
+            
+            NSLog(@"\nusername = %@,\n usid = %@,\n token = %@ iconUrl = %@,\n unionId = %@,\n ",snsAccount.userName,snsAccount.usid,snsAccount.accessToken,snsAccount.iconURL, snsAccount.unionId);
+            
+        }});
+    
+    
 }
 
 
@@ -71,6 +102,19 @@
         [_sharedButton addTarget:self action:@selector(sharedByUMSocial) forControlEvents:UIControlEventTouchUpInside];
     }
     return _sharedButton;
+}
+
+- (UIButton *)loginButton{
+    if (_loginButton == nil) {
+        _loginButton = [[UIButton alloc] init];
+        _loginButton.layer.borderColor = [UIColor js_colorWithHex:0xFF1493].CGColor;
+        _loginButton.layer.borderWidth = 2;
+        [_loginButton setTitle:@"第三方登录" forState:UIControlStateNormal];
+        [_loginButton setTitleColor:[UIColor js_randomColor] forState:UIControlStateNormal];
+        [_loginButton addTarget:self action:@selector(singleSignOnBySina) forControlEvents:UIControlEventTouchUpInside];
+        
+    }
+    return _loginButton;
 }
 
 - (void)didReceiveMemoryWarning {
