89ab0d92f6370ec677dfe9a334cc7a8e6904fa83
ShenYj <shenyj4@51nb.com>
2016-09-11 12:24:13 +0800

    set up UI

--- Demos/Demos/Classes/02-视图搭建/通讯录/Controller/JSContactViewController.m
+++ Demos/Demos/Classes/02-视图搭建/通讯录/Controller/JSContactViewController.m
@@ -10,6 +10,8 @@
 
 @interface JSContactViewController ()
 
+@property (nonatomic,strong) UIButton *contactButton;
+
 @end
 
 @implementation JSContactViewController
@@ -25,11 +27,30 @@
     
     self.view.backgroundColor = [UIColor whiteColor];
     
+    [self.view addSubview:self.contactButton];
+    
+    [self.contactButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.centerY.mas_equalTo(self.view);
+        make.size.mas_equalTo(CGSizeMake(100, 40));
+    }];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - lazy
+
+- (UIButton *)contactButton {
+    
+    if (_contactButton == nil) {
+        _contactButton = [[UIButton alloc] init];
+        [_contactButton setTitle:@"打开通讯录" forState:UIControlStateNormal];
+        [_contactButton setTitleColor:[UIColor js_randomColor] forState:UIControlStateNormal];
+    }
+    return _contactButton;
 }
 
 /*
