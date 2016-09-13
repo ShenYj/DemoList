0c7698d23a65527eb8fca46ecd0eed0f6bc0a01e
ShenYj <shenyj4@51nb.com>
2016-09-13 23:58:05 +0800

    播放音效

--- Demos/Demos/Classes/03-多媒体类/播放音效/View/JSPlaySoundButton.m
+++ Demos/Demos/Classes/03-多媒体类/播放音效/View/JSPlaySoundButton.m
@@ -7,6 +7,12 @@
 //
 
 #import "JSPlaySoundButton.h"
+#import <AVFoundation/AVFoundation.h>
+
+@interface JSPlaySoundButton ()
+// 音效缓存
+@property (nonatomic,strong) NSMutableDictionary *soundIdCache;
+@end
 
 @implementation JSPlaySoundButton
 
@@ -30,6 +36,48 @@
 
 - (void)clickPlayButton:(UIButton *)sender {
     
+    NSString *fileName = @"buyao.caf";
+    
+    [self playSystemSoundWithFileName:fileName];
+    
+}
+
+
+- (void)playSystemSoundWithFileName:(NSString *)fileName{
+    
+    // AudioServicesPlayAlertSound(<#SystemSoundID inSystemSoundID#>)  手机设置静音时会震动
+    
+    // 设置自定义声音文件的路径
+    NSString *filePath = [[NSBundle mainBundle]pathForResource:fileName ofType:nil];
+    NSURL *fileUrl = [NSURL fileURLWithPath:filePath];
+    
+    // 从缓存中获取
+    SystemSoundID soundID = [[self.soundIdCache objectForKey:fileName] unsignedIntValue];
+    // 判断是否存在
+    if (soundID == 0) {
+        
+        // 生成soundID 音效的唯一标识符  (防止每次都去生成(经常使用),消耗性能,所以使用缓存记录)
+        AudioServicesCreateSystemSoundID((__bridge CFURLRef _Nonnull)(fileUrl), &soundID);
+        
+        // 添加到缓存中
+        [self.soundIdCache setObject:@(soundID) forKey:fileName];
+        NSLog(@"添加到缓存");
+    }
+    
+    // AudioServicesPlaySystemSound 播放系统音效
+    AudioServicesPlaySystemSound(soundID);
+    
+}
+
+#pragma mark - 缓存懒加载
+
+- (NSMutableDictionary *)soundIdCache{
+    
+    if (_soundIdCache == nil) {
+        _soundIdCache = [NSMutableDictionary dictionary];
+        NSLog(@"生成缓存");
+    }
+    return _soundIdCache;
 }
 
 @end
