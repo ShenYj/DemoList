b34468f1f999a34f11f29aa9a3d49b1f6c65e8e3
ShenYj <shenyj4@51nb.com>
2016-09-14 21:37:16 +0800

    UI

--- Demos/Demos/Classes/01-逻辑处理/指纹识别/Controller/JSBiometricViewController.m
+++ Demos/Demos/Classes/01-逻辑处理/指纹识别/Controller/JSBiometricViewController.m
@@ -7,10 +7,11 @@
 //
 
 #import "JSBiometricViewController.h"
+#import <LocalAuthentication/LocalAuthentication.h>
 
 @interface JSBiometricViewController ()
 
-@property (nonatomic,strong) UILabel *decriptionLabel;
+@property (nonatomic,strong) UILabel *descriptionLabel;
 
 @end
 
@@ -26,18 +27,34 @@
 - (void)prepareView{
     
     self.view.backgroundColor = [UIColor whiteColor];
+
+    [self.view addSubview:self.descriptionLabel];
+    
+    [self.descriptionLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(self.view).mas_offset(70);
+        make.left.right.mas_equalTo(self.view);
+        make.height.mas_equalTo(100);
+    }];
+    
+    
 }
 
 
 #pragma mark - lazy
 
 
-- (UILabel *)decriptionLabel {
+- (UILabel *)descriptionLabel {
     
-    if (_decriptionLabel == nil) {
-        _decriptionLabel = [[UILabel alloc] init];
+    if (_descriptionLabel == nil) {
+        _descriptionLabel = [[UILabel alloc] init];
+        _descriptionLabel.font = [UIFont systemFontOfSize:16];
+        _descriptionLabel.numberOfLines = 0;
+        _descriptionLabel.textAlignment = NSTextAlignmentCenter;
+        _descriptionLabel.backgroundColor = [UIColor js_colorWithHex:0xFF69B4];
+        _descriptionLabel.textColor = [UIColor js_colorWithHex:0x1E90FF];
+        _descriptionLabel.text = @"演示指纹识别使用:\n点击按钮,进行指纹验证.";
     }
-    return _decriptionLabel;
+    return _descriptionLabel;
 }
 
 
