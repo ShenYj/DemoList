461d17e66887c34d6725898917a7426d14ebe073
ShenYj <shenyj4@51nb.com>
2016-09-15 16:15:31 +0800

    update UI

--- Demos/Demos/Classes/01-逻辑处理/短信验证/Controller/JSMobViewController.m
+++ Demos/Demos/Classes/01-逻辑处理/短信验证/Controller/JSMobViewController.m
@@ -7,13 +7,16 @@
 //
 
 #import "JSMobViewController.h"
+#import <SMS_SDK/SMSSDK.h>
 
 @interface JSMobViewController ()
 
 @property (nonatomic,strong) UILabel *detailLabel;
 @property (nonatomic,strong) UILabel *resultLabel;
 @property (nonatomic,strong) UITextField *phoneNumberInputTextField;
-@property (nonatomic,strong) UIButton *registerButton;
+@property (nonatomic,strong) UITextField *authenCodeInputTextField;
+@property (nonatomic,strong) UIButton *getCodeButton;
+@property (nonatomic,strong) UIButton *authenButton;
 
 @end
 
@@ -26,52 +29,91 @@
     [self prepareView];
 }
 
-
-
 - (void)prepareView {
     
     self.view.backgroundColor = [UIColor whiteColor];
     
     [self.view addSubview:self.detailLabel];
     [self.view addSubview:self.phoneNumberInputTextField];
+    [self.view addSubview:self.authenCodeInputTextField];
     [self.view addSubview:self.resultLabel];
-    [self.view addSubview:self.registerButton];
+    [self.view addSubview:self.authenButton];
+    [self.view addSubview:self.getCodeButton];
     
     [self.detailLabel mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.mas_equalTo(self.view).mas_offset(100);
-        make.height.mas_equalTo(80);
         make.left.right.mas_equalTo(self.view);
+        make.height.mas_equalTo(100);
     }];
     
-    [self.phoneNumberInputTextField mas_makeConstraints:^(MASConstraintMaker *make) {
+    NSArray *textFields = @[self.phoneNumberInputTextField,self.authenCodeInputTextField];
+    [textFields mas_distributeViewsAlongAxis:MASAxisTypeHorizontal withFixedSpacing:30 leadSpacing:20 tailSpacing:30];
+    [textFields mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.mas_equalTo(self.detailLabel.mas_bottom).mas_offset(20);
-        make.centerX.mas_equalTo(self.view);
-        make.width.mas_equalTo(200);
         make.height.mas_equalTo(30);
     }];
     
-    [self.registerButton mas_makeConstraints:^(MASConstraintMaker *make) {
+    
+    NSArray *buttons = @[self.getCodeButton,self.authenButton];
+    [buttons mas_distributeViewsAlongAxis:MASAxisTypeHorizontal withFixedSpacing:30 leadSpacing:20 tailSpacing:30];
+    [buttons mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.height.mas_equalTo(44);
         make.top.mas_equalTo(self.phoneNumberInputTextField.mas_bottom).mas_offset(20);
-        make.size.mas_equalTo(CGSizeMake(100, 44));
-        make.centerX.mas_equalTo(self.view);
     }];
+    
     
     [self.resultLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.mas_equalTo(self.registerButton.mas_bottom).mas_offset(20);
-        make.centerX.mas_equalTo(self.view);
-        make.width.mas_equalTo(100);
-        make.height.mas_equalTo(30);
+        make.top.mas_equalTo(self.authenButton.mas_bottom).mas_offset(20);
+        make.left.right.mas_equalTo(self.view);
+        make.bottom.mas_equalTo(self.view).mas_offset(-100);
     }];
+    
+    
 
 }
 
 #pragma mark - target
 
-- (void)clickRegisterButton:(UIButton *)sender {
- 
+- (void)clickGetCodeButton:(UIButton *)sender {
+    
+    /**
+     *  @from                    v1.1.1
+     *  @brief                   获取验证码(Get verification code)
+     *
+     *  @param method            获取验证码的方法(The method of getting verificationCode)
+     *  @param phoneNumber       电话号码(The phone number)
+     *  @param zone              区域号，不要加"+"号(Area code)
+     *  @param customIdentifier  自定义短信模板标识 该标识需从官网http://www.mob.com上申请，审核通过后获得。(Custom model of SMS.  The identifier can get it  from http://www.mob.com  when the application had approved)
+     *  @param result            请求结果回调(Results of the request)
+     */
+    
+    [SMSSDK getVerificationCodeByMethod:SMSGetCodeMethodSMS phoneNumber:self.phoneNumberInputTextField.text zone:@"86" customIdentifier:nil result:^(NSError *error) {
+        
+        if (!error) {
+            NSLog(@"获取验证码成功");
+            
+        } else {
+            NSLog(@"错误信息：%@",error);
+        }
+    }];
+    
     
 }
 
+- (void)clickRegisterButton:(UIButton *)sender {
+    
+    [SMSSDK commitVerificationCode:self.authenCodeInputTextField.text phoneNumber:self.phoneNumberInputTextField.text zone:@"86" result:^(NSError *error) {
+        
+        if (!error) {
+            NSLog(@"验证成功");
+        }
+        else
+        {
+            NSLog(@"错误信息:%@",error);
+        }
+    }];
+    
+}
 
 #pragma mark - lazy
 
@@ -80,9 +122,10 @@
     if (_detailLabel == nil) {
         _detailLabel = [[UILabel alloc] init];
         _detailLabel.font = [UIFont systemFontOfSize:16];
-        _detailLabel.text = @"输入手机号,点击\"短信验证\"按钮进行验证";
+        _detailLabel.text = @"输入手机号,点击\"短信验证\"按钮进行验证\n注意:\n这里并没有对手机号等逻辑进行处理\n需要填入正确手机号,点击获取验证码后再进行验证";
+        _detailLabel.numberOfLines = 0;
         _detailLabel.textAlignment = NSTextAlignmentCenter;
-        _detailLabel.layer.borderColor = [UIColor lightGrayColor].CGColor;
+        _detailLabel.layer.borderColor = [UIColor redColor].CGColor;
         _detailLabel.layer.borderWidth = 2;
         _detailLabel.textColor = [UIColor js_colorWithHex:0x90EE90];
     }
@@ -93,6 +136,8 @@
     
     if (_resultLabel == nil) {
         _resultLabel = [[UILabel alloc] init];
+        _resultLabel.text = @"验证结果...";
+        _resultLabel.textAlignment = NSTextAlignmentCenter;
         _resultLabel.font = [UIFont systemFontOfSize:18];
         _resultLabel.layer.borderColor = [UIColor redColor].CGColor;
         _resultLabel.layer.borderWidth = 2;
@@ -112,19 +157,45 @@
     return _phoneNumberInputTextField;
 }
 
-- (UIButton *)registerButton {
+- (UITextField *)authenCodeInputTextField {
     
-    if (_registerButton == nil) {
-        _registerButton = [[UIButton alloc] init];
-        [_registerButton setTitle:@"短信验证" forState:UIControlStateNormal];
-        _registerButton.layer.borderColor = [UIColor blackColor].CGColor;
-        _registerButton.layer.borderWidth = 1;
-        [_registerButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-        [_registerButton setTitleColor:[UIColor lightGrayColor] forState:UIControlStateHighlighted];
-        [_registerButton addTarget:self action:@selector(clickRegisterButton:) forControlEvents:UIControlEventTouchUpInside];
+    if (_authenCodeInputTextField == nil) {
+        _authenCodeInputTextField = [[UITextField alloc] init];
+        _authenCodeInputTextField.placeholder = @"在这里输入验证码...";
+        _authenCodeInputTextField.layer.borderColor = [UIColor lightGrayColor].CGColor;
+        _authenCodeInputTextField.layer.borderWidth = 2;
     }
-    return _registerButton;
+    return _authenCodeInputTextField;
+}
+
+- (UIButton *)authenButton {
     
+    if (_authenButton == nil) {
+        _authenButton = [[UIButton alloc] init];
+        [_authenButton setTitle:@"短信验证" forState:UIControlStateNormal];
+        _authenButton.layer.borderColor = [UIColor blackColor].CGColor;
+        _authenButton.layer.borderWidth = 1;
+        [_authenButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [_authenButton setTitleColor:[UIColor lightGrayColor] forState:UIControlStateHighlighted];
+        [_authenButton addTarget:self action:@selector(clickRegisterButton:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _authenButton;
+    
+}
+
+- (UIButton *)getCodeButton {
+    
+    if (_getCodeButton == nil) {
+        _getCodeButton = [[UIButton alloc] init];
+        [_getCodeButton setTitle:@"获取验证码" forState:UIControlStateNormal];
+        _getCodeButton.layer.borderColor = [UIColor blackColor].CGColor;
+        _getCodeButton.layer.borderWidth = 1;
+        [_getCodeButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [_getCodeButton setTitleColor:[UIColor lightGrayColor] forState:UIControlStateHighlighted];
+        [_getCodeButton addTarget:self action:@selector(clickGetCodeButton:) forControlEvents:UIControlEventTouchUpInside];
+        
+    }
+    return _getCodeButton;
 }
 
 @end
