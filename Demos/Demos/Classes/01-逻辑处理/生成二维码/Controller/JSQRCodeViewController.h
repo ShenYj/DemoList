b93e5aaced55a18519be65e00eef00c3c4882788
ShenYj <shenyj4@51nb.com>
2016-10-09 21:33:11 +0800

    生成二维码

---
+++ Demos/Demos/Classes/01-逻辑处理/生成二维码/Controller/JSQRCodeViewController.h
@@ -0,0 +1,13 @@
+//
+//  JSQRCodeViewController.h
+//  Demos
+//
+//  Created by ShenYj on 2016/10/9.
+//  Copyright © 2016年 ___ShenYJ___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSQRCodeViewController : UIViewController
+
+@end
