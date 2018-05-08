15e60970878f3859bca4cd40d9840f01a3fef38e
ShenYj <shenyj4@51nb.com>
2018-05-08 22:49:08 +0800

    MyLayoutSize.wrap

--- Demos/Demos/Classes/02-视图搭建/MyLayout约束/Controllers/SubVC/JSMyTableLayout.m
+++ Demos/Demos/Classes/02-视图搭建/MyLayout约束/Controllers/SubVC/JSMyTableLayout.m
@@ -6,6 +6,8 @@
 //  Copyright © 2018年 ___ShenYJ___. All rights reserved.
 //
 
+
+
 #import "JSMyTableLayout.h"
 
 @interface JSMyTableLayout ()
@@ -18,6 +20,48 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self setUpMyTableLayoutView];
+//    [self demo];
+}
+
+- (void)demo {
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.title                = [NSStringFromClass(self.class) substringFromIndex:2];
+    
+    MyTableLayout *S = [MyTableLayout tableLayoutWithOrientation:MyOrientation_Vert];
+    S.wrapContentWidth = YES;
+    S.myLeft = 20;
+    S.myTop = 100;
+    S.padding = UIEdgeInsetsMake(10, 10, 10, 10);
+    S.subviewHSpace = 10;
+    S.subviewVSpace = 10;
+    
+    [S addRow:MyLayoutSize.wrap colSize:MyLayoutSize.wrap];
+    
+    UIView *A = [UIView new];
+    A.mySize = CGSizeMake(50,40);
+    [S addSubview:A];
+    
+    UIView *B = [UIView new];
+    B.mySize = CGSizeMake(100,40);
+    [S addSubview:B];
+    
+    UIView *C = [UIView new];
+    C.mySize = CGSizeMake(30,40);
+    [S addSubview:C];
+    
+    [S addRow:MyLayoutSize.wrap colSize:MyLayoutSize.wrap];
+    
+    UIView *D = [UIView new];
+    D.mySize = CGSizeMake(200,40);
+    [S addSubview:D];
+    
+    [self.view addSubview:S];
+    S.backgroundColor = [UIColor redColor];
+    A.backgroundColor = [UIColor greenColor];
+    B.backgroundColor = [UIColor blueColor];
+    C.backgroundColor = [UIColor orangeColor];
+    D.backgroundColor = [UIColor cyanColor];
 }
 
 - (void)setUpMyTableLayoutView {
@@ -27,14 +71,14 @@
     MyTableLayout *tableLayout = [MyTableLayout tableLayoutWithOrientation:MyOrientation_Vert];
     tableLayout.backgroundColor = [UIColor js_randomColor];
     tableLayout.wrapContentWidth = YES;
-    tableLayout.myHorzMargin = 20;
-    tableLayout.myVertMargin = 100;
+    tableLayout.myTop = 100;
+    tableLayout.myCenterX = 0;
     tableLayout.subviewHSpace = 10;
     tableLayout.subviewVSpace = 10;
     [self.view addSubview:tableLayout];
     
     // row1
-    [tableLayout addRow:80 colSize:40];
+    [tableLayout addRow:MyLayoutSize.wrap colSize:MyLayoutSize.wrap];
     
     UIView *redView = [[UIView alloc] init];
     redView.backgroundColor = [UIColor redColor];
@@ -52,7 +96,7 @@
     [tableLayout addSubview:yellowView];
     
     // row2
-    [tableLayout addRow:50 colSize:150];
+    [tableLayout addRow:MyLayoutSize.wrap colSize:MyLayoutSize.wrap];
     
     UIView *greenView = [[UIView alloc] init];
     greenView.backgroundColor = [UIColor greenColor];
@@ -60,7 +104,7 @@
     [tableLayout addSubview:greenView];
     
     // row3
-    [tableLayout addRow:80 colSize:70];
+    [tableLayout addRow:MyLayoutSize.wrap colSize:MyLayoutSize.wrap];
     
     UIView *cyanView = [[UIView alloc] init];
     cyanView.backgroundColor = [UIColor cyanColor];
